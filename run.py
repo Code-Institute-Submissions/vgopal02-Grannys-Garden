48c317c158088079edf8e6b3180bae433f4f6594
vgopal02 <rajeev.vidya@outlook.com>
2021-10-12 08:27:27 +0000

    Add introduction storyline prompts

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+
+a = 2
+
+
+def start():
+    # Storyline prompts
+    print("WELCOME TO GRANNY'S GARDEN\n")
+    print("\n Granny was in the garden picking apples....")
+    time.sleep(a)
+    print("\n when the wicked witch from across the mountain kidnapped her!")
+    time.sleep(a)
+    print("\n Granny needs to be rescued...")
+    time.sleep(a)
+    print("\n DO YOU WANT TO SAVE GRANNY? (y or n)")
