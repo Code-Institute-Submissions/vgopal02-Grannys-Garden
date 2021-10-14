2123b3c6c4cca11b6b8efd68cc3e0b8c083be26c
vgopal02 <rajeev.vidya@outlook.com>
2021-10-14 20:13:50 +0000

    Add options to process in monster_den()

--- run.py
+++ run.py
@@ -107,6 +107,20 @@ def monster_den():
     time.sleep(a)
     print("Door 2 is closed but big enough for you to fit through")
     time.sleep(a)
+    print("Which door will you choose to take (1 or 2)")
+    print("1). Squeeze through the small window")
+    print("2). Open the bigger window and comfortably slide through")
+    # convert the player's input() to lower_case
+    answer = input(">").lower()
+    # if player typed "1" dungeon_den()
+    if answer == "1":
+        dungeon_den()
+    # else if player typed "2" lead him to game_over()
+    elif answer == "2":
+        game_over()
+    # else return to start()
+    else:
+        start()
 
 # start the game
 start()
